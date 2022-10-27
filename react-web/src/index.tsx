6761dfa9a2aeb6cae5d8eb5c16278bb42daa9a74
amnambiar <anu.sripuri@gmail.com>
2022-10-27 19:50:07 +0530

    Removed unused code

--- react-web/src/index.tsx
+++ react-web/src/index.tsx
@@ -20,29 +20,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-/**
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC_HhfD7eItpV2U17Dn1PZsDVmEXPBJgy4",
-  authDomain: "iohk-dapps-certification.firebaseapp.com",
-  projectId: "iohk-dapps-certification",
-  storageBucket: "iohk-dapps-certification.appspot.com",
-  messagingSenderId: "1092766446086",
-  appId: "1:1092766446086:web:75cf7870e51b5f303c0987",
-  measurementId: "G-5MJ9SVX7HV"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
- */
