8225123e21030723d5fbadc206f2bd5e59824e44
amnambiar <anu.sripuri@gmail.com>
2022-11-10 15:35:33 +0530

    trials with connect wallet

--- react-web/src/app/App.tsx
+++ react-web/src/app/App.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, Outlet } from "react-router-dom";
 import { BASE_URL } from "constants/route";
 
 import "./App.scss";
 import Certification from "pages/certification/Certification";
+import Button from "components/Button/Button";
+import ConnectWallet from "components/ConnectWallet/ConnectWallet";
 
 const PageLayout = () => {
+
+  const [connectToWallet, setConnectToWallet] = useState(false)
+  const openConnectWallet = () => {
+    setConnectToWallet(true)
+  }
+  
   return (
     <>
-      <header></header>
+      <header>
+        <Button
+            type="button"
+            className="btn btn-primary"
+            buttonLabel={"Connect Wallet"}
+            onClick={(_) => openConnectWallet()}
+          />
+      </header>
 
       {/* Load page content here */}
       <section data-testid="contentWrapper" id="contentWrapper">
         <Outlet />
       </section>
+
+      {connectToWallet ? <>
+        <ConnectWallet />
+      </> : null}
     </>
   );
 };
