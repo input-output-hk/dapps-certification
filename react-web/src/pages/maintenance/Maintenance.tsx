047de0ba95996bbc6694037bfd97a45cb97f6342
amnambiar <anu.sripuri@gmail.com>
2022-12-04 14:26:13 +0530

    Text fix

--- react-web/src/pages/maintenance/Maintenance.tsx
+++ react-web/src/pages/maintenance/Maintenance.tsx
@@ -5,7 +5,6 @@ function MaintenancePage() {
   return (
     <div className="maintenance-page-container">
       <h2>Coming soon...</h2>
-      <h3>Site is under construction...</h3>
     </div>
   );
 }
