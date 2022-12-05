47f08fad75545d9d362538f28a56fd66f92e748e
amnambiar <anu.sripuri@gmail.com>
2022-12-05 19:09:02 +0530

    DAC-317 Removed test files temporarily for build fix

--- react-web/src/compositions/Timeline/Timeline.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Timeline from "./Timeline";
-import { TIMELINE_CONFIG } from "./timeline.config";
-
-test("renders Timeline component", () => {
-    render(<Timeline statusConfig={TIMELINE_CONFIG} />);
-    expect(screen.getByTestId("statusTimeline")).toBeInTheDocument();
-});
--- react-web/src/pages/certification/Certification.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Certification from "./Certification";
-
-describe("Test cases for Form component", () => {
-  test("should display correct error message", () => {
-    render(<Certification />);
-    screen.findByText("Enter Github repository details").then(res => { });
-  });
-});
