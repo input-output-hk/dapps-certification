795be414a53e6887b8c4684d6b84350f16cb6ffc
Bogdan Manole <bogdan.manole@iohk.io>
2023-09-06 12:15:53 +0000

    feat: add lace support (PLT-5598)
    - modify Signature decodding based on how Lace is codding it
    - for signature verification construct the Signed payload based on the Signature headers
    - fix the skipped verification of the signature when the returned values is Right False
    - add complete signature tests
    
    refs: [PLT-5598](https://input-output.atlassian.net/browse/PLT-5598)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,8 @@
 import Test.Hspec
 import qualified ProfileWalletSpec as ProfileWallet
+import qualified SignatureSpec as Signature
 
 main :: IO ()
-main = hspec ProfileWallet.spec
+main = hspec $ do
+  ProfileWallet.spec
+  Signature.spec
