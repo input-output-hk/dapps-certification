0fb0bfbbce4d67b8b19c702c85de42fac6d9787b
zeme <lorenzo.calegari@iohk.io>
2023-11-10 15:39:35 +0100

    Bump IOGX to V4

--- nix/ci.nix
+++
@@ -1,8 +0,0 @@
-# This file is part of the IOGX template and is documented at the link below:
-# https://www.github.com/input-output-hk/iogx#39-nixcinix
-
-{
-  includedPaths = [
-    "apps.dockerApps.evaluation-test"
-  ];
-}
